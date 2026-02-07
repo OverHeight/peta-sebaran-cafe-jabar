702a812af11bff082cefbe6fb9d492b35afad678
OverHeight <109268126+OverHeight@users.noreply.github.com>
2026-02-07 03:39:33 +0000

    minor changes

--- data/filter.py
+++ data/filter.py
@@ -22,8 +22,10 @@ def reverse_geocode(lat, lon):
 def main():
     df = pd.read_csv(INPUT_CSV)
 
-    df["alamat_asli"] = ""
+    if 'nama' in df.columns:
+        df = df[df['nama'].str.strip().str.lower() != 'tanpa nama'].reset_index(drop=True)
 
+    df["alamat_asli"] = ""
     total = len(df)
     print(f"Reverse geocoding {total} data \n")
 
@@ -31,18 +33,24 @@ def main():
         lat = row["lat"]
         lon = row["lon"]
         address = reverse_geocode(lat, lon)
+        
         df.at[i, "alamat_asli"] = address
 
         if (i + 1) % 10 == 0 or i == 0:
             print(f"Progress: {i + 1}/{total}")
 
-        # rate limit soalnya nomatim ngasih batas 1 request per detik
+        # Rate limit nomatim cuma 1/s
         time.sleep(1) 
-        
-    print("Saved to:", OUTPUT_CSV)
 
+    # Cleanup
+    initial_len = len(df)
+    df = df[
+        (df["alamat_asli"].str.strip().str.lower() != "tanpa nama") & 
+        (df["alamat_asli"] != "")
+    ]
+
+    print("File Save:", OUTPUT_CSV)
     df.to_csv(OUTPUT_CSV, index=False)
-
 
 if __name__ == "__main__":
     main()
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,34 +3,35 @@ import pandas as pd
 import folium
 import altair as alt
 from streamlit_folium import st_folium
+from folium.plugins import MarkerCluster
 st.set_page_config(page_title="Peta Café Jabar", layout="wide")
 
-with st.sidebar:
-    st.title("⚙️ Pengaturan")
-    uploaded_file = st.file_uploader("Unggah CSV Lokasi", type=['csv'])
-    st.info("Pastikan CSV memiliki kolom: nama, wilayah, lat, lon, dan sumber.")
-    process_btn = st.button("Proses Data")
+# with st.sidebar:
+#     st.title("⚙️ Pengaturan")
+#     uploaded_file = st.file_uploader("Unggah CSV Lokasi", type=['csv'])
+#     st.info("Pastikan CSV memiliki kolom: nama, wilayah, lat, lon, dan sumber.")
+#     process_btn = st.button("Proses Data")
 
-if uploaded_file:
-    df = pd.read_csv(uploaded_file)
-else:
-    try:
-        df = pd.read_csv("./data/spatial/coffee_shop_jawa_barat_with_address.csv")
-    except:
-        data = {
-            'nama': ['Starbucks', 'SPBU Dago', 'Tanpa Nama', 'Warung Makan', 'Habenagen Coffeeshop', 'The Oldway Cafe', 'Kabut Salju', 'Cokotetra', 'YUTAKANA'],
-            'wilayah': ['Kota Bandung', 'Kota Bandung', 'Kabupaten Bandung', 'Sumedang', 'Kota Bandung', 'Kota Bandung', 'Kota Bandung', 'Kota Bandung', 'Kota Bandung'],
-            'lat': [-6.894, -6.869, -6.8644, -6.892, -6.9021, -6.907, -6.9183, -6.8785, -6.8835],
-            'lon': [107.6055, 107.6209, 107.6279, 107.7641, 107.6041, 107.6231, 107.6143, 107.6168, 107.6147],
-            'sumber': ['Google Maps', 'Google Maps', 'Manual', 'Survey', 'Google Maps', 'Google Maps', 'Google Maps', 'Google Maps', 'Google Maps']
-        }
-        df = pd.DataFrame(data)
+# if uploaded_file:
+#   df = pd.read_csv(uploaded_file)
+# else:
+#     try:
+df = pd.read_csv("./data/spatial/coffee_shop_jawa_barat_final.csv")
+#     except:
+#         data = {
+#             'nama': ['Starbucks', 'SPBU Dago', 'Tanpa Nama', 'Warung Makan', 'Habenagen Coffeeshop', 'The Oldway Cafe', 'Kabut Salju', 'Cokotetra', 'YUTAKANA'],
+#             'wilayah': ['Kota Bandung', 'Kota Bandung', 'Kabupaten Bandung', 'Sumedang', 'Kota Bandung', 'Kota Bandung', 'Kota Bandung', 'Kota Bandung', 'Kota Bandung'],
+#             'lat': [-6.894, -6.869, -6.8644, -6.892, -6.9021, -6.907, -6.9183, -6.8785, -6.8835],
+#             'lon': [107.6055, 107.6209, 107.6279, 107.7641, 107.6041, 107.6231, 107.6143, 107.6168, 107.6147],
+#             'sumber': ['Google Maps', 'Google Maps', 'Manual', 'Survey', 'Google Maps', 'Google Maps', 'Google Maps', 'Google Maps', 'Google Maps']
+#         }
+#         df = pd.DataFrame(data)
 
-if 'sumber' not in df.columns:
-    df['sumber'] = 'Data Internal'
+# if 'sumber' not in df.columns:
+#     df['sumber'] = 'Data Internal'
 
 
-st.title("☕ Dashboard Sebaran Café Jawa Barat")
+st.title("☕ Peta Sebaran Café Jawa Barat")
 st.markdown("---")
 
 
@@ -39,14 +40,20 @@ col_map, col_table = st.columns([1.8, 1.2])
 with col_map:
     st.subheader("Peta Sebaran Lokasi")
     m = folium.Map(location=[df['lat'].mean(), df['lon'].mean()], zoom_start=11)
+    
+    cluster = MarkerCluster().add_to(m)
+
     for _, row in df.iterrows():
         folium.Marker(
-            [row['lat'], row['lon']], 
-            popup = f"**{row['nama']}**<br>Wilayah: {row['wilayah']}",
-            tooltip=row['nama'],
+            location=[row['lat'], row['lon']],
+            popup=f"""
+            <b>{row['nama']}</b><br><br>
+            Wilayah: {row['wilayah']}<br>
+            Alamat: {row.get("alamat_asli")}
+            """,
             icon=folium.Icon(color='orange', icon='coffee', prefix='fa')
-        ).add_to(m)
-    st_folium(m, width='100%', height=450)
+        ).add_to(cluster)
+    st_folium(m, width='110%', height=450)
 
 with col_table:
     st.subheader("Dataset Lokasi Café")
@@ -70,7 +77,7 @@ with col_pie1:
 
     chart_wilayah = alt.Chart(wilayah_counts).mark_arc(innerRadius=50).encode(
         theta=alt.Theta(field="Jumlah", type="quantitative"),
-        color=alt.Color(field="Wilayah", type="nominal", legend=alt.Legend(orient="bottom", columns=1)),
+        color=alt.Color(field="Wilayah", type="nominal"),
         tooltip=['Wilayah', 'Jumlah']
     ).properties(height=350, title="Proporsi per Wilayah")
     st.altair_chart(chart_wilayah, use_container_width=True)
@@ -82,7 +89,7 @@ with col_pie2:
 
     chart_sumber = alt.Chart(sumber_counts).mark_arc(innerRadius=50).encode(
         theta=alt.Theta(field="Jumlah", type="quantitative"),
-        color=alt.Color(field="Sumber", type="nominal", scale=alt.Scale(scheme='pastel1'), legend=alt.Legend(orient="bottom")),
+        color=alt.Color(field="Sumber", type="nominal", legend=alt.Legend(orient="bottom")),
         tooltip=['Sumber', 'Jumlah']
     ).properties(height=350, title="Proporsi per Sumber")
     st.altair_chart(chart_sumber, use_container_width=True)
